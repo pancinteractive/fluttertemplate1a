5f4ef4cb9f1e393592a33f52e91f34f8ccdc177d
Alex <pancsoft@gmail.com>
2021-12-04 14:10:55 -0500

    Initial commit

---
+++ lib/main_production.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_template/config/pre_app_config.dart';
+import 'package:flutter_template/log/bloc_events_logger.dart';
+
+import 'app.dart';
+import 'config/flavor_config.dart';
+import 'config/network_constants.dart';
+
+Future<void> main() async {
+  FlavorConfig.set(
+    Flavor.PRODUCTION,
+    FlavorValues(
+      baseUrlApi: baseUrlProd + apiPrefix,
+    ),
+  );
+
+  await preAppConfig();
+
+  BlocOverrides.runZoned(() => runApp(App()), blocObserver: BlocEventsLogger());
+}
